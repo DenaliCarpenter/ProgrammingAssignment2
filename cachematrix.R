9a35eb1a446008cd7e3a7a264fcc6ce91f269776
Denali Carpenter <denali.carpenter@inteleos.org>
2022-11-29 00:19:18 -0600

    Add solution to ProgrammingAssignment2, makeCacheMatrix & cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  I <- NULL
+  set <- function(y){
+    x <<- y
+    I <- NULL
+  }
+  get <- function() x
+  setI <- function(solve) x <<- solve
+  getI <- function() I
+  list(set = set,
+       get = get,
+       setI = setI,
+       getI = getI)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  I <- x$getI()
+  if(!is.null(I)) {
+    message("getting cached inverse, I")
+    return(I)
+  }
+  data <- x$get()
+  I <- solve(data, ...)
+  x$setI(I)
+  I
 }
